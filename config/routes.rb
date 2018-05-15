5804a71476a4790ec19ab1879833b6669e4003c2
Sarah Woertler <sarah.woertler@gmail.com>
2018-05-15 15:26:53 +0200

    Number 1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #list all tasks
+  get '/tasks', to: 'tasks#index'
+
+
+  #create a restaurant
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  post '/tasks', to: 'tasks#create'
+
+  #show one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
+
+  #update a post
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch '/tasks/:id', to: 'tasks#update'
+
+  #delete a task
+  delete '/tasks/:id', to: 'tasks#destroy'
+
 end
